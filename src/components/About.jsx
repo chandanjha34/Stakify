8b3f83f0631125c60820238c65e71b8d5e75d8cb
jhachanda <jha95375@gmail.com>
2024-11-04 17:37:21 +0530

    About page completed

---
+++ src/components/About.jsx
@@ -0,0 +1,71 @@
+import React, { useEffect,useRef } from "react";
+import {gsap} from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import ship from '../assets/About_Assets/ship.png';
+gsap.registerPlugin(ScrollTrigger);
+function About(){
+    const imgRef=useRef(null);
+    const aboutRef=useRef(null);
+
+    useEffect(()=>{
+        const section=aboutRef.current;
+        gsap.fromTo(imgRef.current,{
+            x:550,
+
+        },
+        {
+            x:0,
+
+            duration:3,
+            ease:"back.out(6.7)",
+            scrollTrigger:{
+                trigger:section,
+                start:"top 0%",
+                end: "top 50%",
+                scrub:4,
+            }
+        }
+    );
+
+    gsap.fromTo(
+        ".sub-text",
+        { x: 40, opacity: 0 },
+        {
+          x: 0,
+          opacity: 1,
+          duration: 0.8,
+          stagger: 0.5,
+          ease: "power2.inOut",
+          scrollTrigger:{
+            trigger:section,
+            start:"top 0%",
+            end: "top 50%",
+            scrub:4,
+        }
+        }
+      );
+
+    return () => {
+        ScrollTrigger.getAll().forEach(trigger => trigger.kill());
+    };
+    },[]);
+
+
+    return(
+        <div ref={aboutRef} className="w-full h-screen bg-[#36343B] overflow-hidden text-white">
+            <div className="text-[300%] font-bold  text-end translate-x-[-35px]" >
+                <h1 className="underline text-[#FF3B3B]">About Stakify</h1>
+            </div>
+            <div className="absolute flex flex-col gap-[20px] w-[60vw] text-[130%]  text-start px-[5vw] leading-[130%] translate-y-[10vh]" >
+                <p className="sub-text">Stakify revolutionizes staking by adding high-stakes betting options, letting users earn both steady rewards and higher-risk returns. <br/> This unique approach appeals to crypto users with varied <br/> risk levels.</p>
+                <p className="sub-text">Stakify’s goal is to build a sustainable reward system where betting losses partly fund staker payouts, creating a fair, <br/> community-driven model. Users can enjoy consistent, lower-risk returns alongside thrilling betting opportunities.</p>
+                <p className="sub-text">With blockchain transparency and smart contracts, Stakify aims to offer a secure, engaging platform that makes staking both rewarding and enjoyable for everyone.</p>
+            </div>
+            <div className="translate-x-[50vw] translate-y-[64vh]">
+                <img ref={imgRef} className="scale-[68%]  translate-y-[-75vh]" src={ship} alt="ship" />
+            </div>
+        </div>
+    );
+}
+
+export default About;
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,31 +6,61 @@ import profile from "../assets/Hero_Assets/profile.png"
 
 function Hero(){
 
-    const navRef=useRef(null);
     useEffect(()=>{
 
-        const elements=navRef.current.querySelectorAll(".nav-items");
-        console.log(elements);
-        gsap.from(elements,{
-            y:40,
-            opacity:0,
-            duration:1,
-            stagger:0.1,
-            ease:"power2.inOut",
+        const mainText = gsap.utils.toArray(".main-text");
+        const subText = gsap.utils.toArray(".sub-text");
+
+        const tl=gsap.timeline();
+        tl.fromTo(
+            ".nav-items",
+            { y: -40, opacity: 0 },
+            {
+              y: 0,
+              opacity: 1,
+              duration: 0.8,
+              stagger: 0.2, // Adjust the stagger value as needed
+              ease: "power2.inOut",
+            }
+          );
+          mainText.forEach((text)=>{
+            tl.fromTo(text, {
+                x: 10,
+                opacity: 0, // Start after main text
+            },
+            {
+                x:0,
+                opacity:1,
+                duration: 0.8,
+                delay: 0.5  
+            }
+        );
         });
-    },[])
-
-
+    
+        subText.forEach((text)=>{
+        tl.fromTo(text, {
+            x: 10,
+            opacity: 0,// Start after main text
+        },
+        {
+            x:0,
+            opacity:1,
+            duration: 0.8,
+            delay: 0.7  
+        }
+    );
+    });
+    },[]);
 
 
     return(
-        <div className="w-screen h-screen bg-[#36343B] overflow-hidden" >
+        <div className="w-full h-screen bg-[#36343B] overflow-hidden" >
             <div className="flex justify-between px-[20px] text-white text-[140%]" >
                 <div className="flex flex-row items-center">
                     <div className=" w-[50px] h-auto" ><img src={logo} alt="logo" /></div>
                     <div className="font-bold text-[120%]" >Stakify</div>
                 </div>
-                <div ref={navRef} className="flex items-center gap-10 hover:cursor-pointer" >
+                <div className="flex items-center gap-10 hover:cursor-pointer" >
                     <div className="nav-items hover:text-[#1AE0E0] rounded-lg px-[10px]">Home</div>
                     <div className="nav-items hover:text-[#1AE0E0] rounded-lg px-[10px]">Guide</div>
                     <div className="nav-items hover:text-[#1AE0E0] rounded-lg px-[10px]">Success</div>
@@ -43,8 +73,8 @@ function Hero(){
                 <div className="w-[60%] h-auto " ><img src={poker} alt="logo" /></div>
             </div>  
             <div className="w-[60vw] h-auto flex flex-col items-start translate-y-[-400px] translate-x-[630px] text-white gap-[10px]" >
-                <p className="text-[380%] font-[Anton] leading-[110%]" >If you're going to <span className="text-[#DC4444]" > bet </span> on something... <br/> bet on yourself...</p> 
-                <p className="text-[20px] font-[Poiret One]" >A platform to stake <span className="text-[#DC4444]" > portfolios </span>  and bet on your <span className="text-[#0A8E13]" > technical analysis </span> </p>
+                <p className="main-text text-[380%] font-[Anton] leading-[110%]" >If you're going to <span className="text-[#DC4444]" > bet </span> on something... <br/> bet on yourself...</p> 
+                <p className="sub-text text-[20px] font-[Poiret One]" >A platform to stake <span className="text-[#DC4444]" > portfolios </span>  and bet on your <span className="text-[#0A8E13]" > technical analysis </span> </p>
                 <div className="flex flex-row gap-10 text-black font-bold" >
                     <button className="border w-[80px] h-[30px] bg-[#DC4444] rounded-lg" >Sign In</button>
                     <button className="border w-[80px] h-[30px] bg-[#0A8E13] rounded-lg" >Sign Up</button>
