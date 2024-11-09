642d99a049e36b165e1e1513eae3ebe01fe759a3
jhachanda <jha95375@gmail.com>
2024-11-09 14:47:52 +0530

    guide section formed

---
+++ src/components/Calculator.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { useEffect,useRef,useState } from "react";
+
+function Calculator(){
+
+    return(
+        <div>
+
+        </div>
+    );
+}
+export default Calculator;
